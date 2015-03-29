86e00456d852330abc0bbaaed257bd265c0eeaed
ErickN <e2@live.com.mx>
2015-03-28 19:42:14 -0600

    Fixed vim config

--- System_configuration/vim_setup.sh
+++ System_configuration/vim_setup.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
 
 #Copyright (C) 2015 Barajas D. Paul <Paul.Barajas@linux.com>
+SOURCE_PATH=`pwd`
+USR_DIR=${HOME}
+VIM_DIR="${USR_DIR}/.vim"
 
-USR_DIR="/home/pi/"
-VIM_DIR="${USR_DIR}.vim"
 
-if [ "${USER}" != "root" ]; then
+sudo -k bash <<EOF
 
-    echo "Please run as root"
-    exit
-fi
+apt-get install -y vim
+apt-get install -y terminator
+apt-get install -y exuberant-ctags
 
-apt-get install vim
-apt-get install terminator
-apt-get install exuberant-ctags
+EOF
 
 if [ ! -d "${VIM_DIR}" ]; then
     mkdir -p "${VIM_DIR}"
 fi
 
-mkdir -p autoload  bitmaps  bundle  colors  plugin
+#mkdir -p autoload  bitmaps  bundle  colors  plugin
 
+cd vim_package/
+git clone git@github.com:tpope/vim-pathogen.git
+git clone git@github.com:tpope/vim-sensible.git
+git clone git@github.com:altercation/vim-colors-solarized.git
+git clone git@github.com:vim-scripts/taglist.vim.git
+cd ..
 echo "installing vim-resource..."
 
 cp -rv vim_package/vim-pathogen/autoload "${VIM_DIR}"
 cp -rv vim_package/vim-sensible/plugin/ "${VIM_DIR}"
 cp -rv vim_package/vim-colors-solarized "${VIM_DIR}"
 cp -rv vim_package/taglist.vim "${VIM_DIR}/plugin"
-cp -rv vim_package/vimrc "${USR_DIR}.vimrc"
+cp -rv vim_package/vimrc "${USR_DIR}/.vimrc"
 
 echo "...finish"
