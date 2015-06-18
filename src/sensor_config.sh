64204eaeabb53b15ca73f1308f70e16c264a4c31
ericknoriega <e2@live.com.mx>
2015-06-17 23:18:09 -0500

    sensor_config.sh central motor python script called

--- src/sensor_config.sh
+++ src/sensor_config.sh
@@ -1,14 +1,26 @@
 #!/bin/sh
 ICOUNTER=0
 PID=""
-
+CBRUSHPID=""
 while [ $ICOUNTER -lt 5 ]
 do
+    python ./BrushlessMod/BrushlessCentral.py ${ICOUNTER}
+    CBRUSHPID=$$
     ./sensor_state.sh &  2> test.txt
     PID=$$
     echo -en "...PID..>>>>>$PID"
     sleep 1m
     kill -9 $PID
+    kill -9 $CBRUSHPID
+    sleep 1s
+    if [ kill -0 $CBRUSHPID ]
+    {
+        kill -9 $CBRUSHPID   
+    }
+    if [ kill -0 $PID ]
+    {
+        kill-9$PID
+    }
     echo -en "......>>>>$ICOUNTER"
     ICOUNTER=$((ICOUNTER+1))
 
