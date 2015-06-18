d9b9edf5f473c086fdb7e562efcd9664f05b293a
ericknoriega <e2@live.com.mx>
2015-06-17 22:23:46 -0500

    Central Brushless DutyCycle Python Script

---
+++ src/BrushlessMod/BrushlessCentral.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+import os.path
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import GPIO_PORT
+
+
+sys.arg[0]=BrushlessCentral.py
+sys.arg[1]= TimeCounter
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(GPIO_PORT.BRUSHLESS_CENTRAL_PWM,GPIO.OUT)
+p = GPIO.PWM(GPIO_PORT.BRUSHLESS_LEFT_PWM,50)
+p.start(4)
+time.sleep(2)
+DutyCycleValue=4.5+(TimeCounter*.4)
+try:
+    while True:
+        p.ChangeDutyCycle(DutyCycleValue)
+        time.sleep(2)
+        if DutyCycleValue == int(8.0):
+            p.stop()
+            GPIO.cleanup()
+            exit()
+ 
+except KeyboardInterrupt:
+    p.stop()
+    GPIO.cleanup()
+
