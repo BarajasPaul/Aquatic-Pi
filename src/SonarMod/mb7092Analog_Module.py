21378a9b9a70b0e9364ccaac94864aa2bd7d4b5b
noriega <e2@live.com.mx>
2015-06-23 04:12:10 +0000

    ServoPlusSonar file

--- src/SonarMod/mb7092Analog_Module.py
+++ src/SonarMod/mb7092Analog_Module.py
@@ -9,7 +9,9 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import GPIO_PORT
 spi = spidev.SpiDev()
 spi.open(0, 0)
- 
+
+Angle = sys.argv[1]
+
 def readadc(adcnum):
 # read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
     if adcnum > 7 or adcnum < 0:
@@ -17,6 +19,15 @@ def readadc(adcnum):
     r = spi.xfer2([1, 8 + adcnum << 4, 0])
     adcout = ((r[1] & 3) << 8) + r[2]
     return adcout
- 
+
 value = readadc(GPIO_PORT.ANALOG_SONAR)
-print (value)
+
+if Angle == 180:
+    f=open("../RESULTS/Sonar180.txt","w")
+    f.write("angle:"+str(Angle)+" Value:"+str(value)+"\n")
+    f.close()
+else:
+    f=open("../RESULTS/Sonar0.txt","w")
+    f.write("angle:"+str(Angle)+" Value:"+str(value)+"\n")
+    f.close()
+exit (0)
